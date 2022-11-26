fd98ef35f1f0a8b4390cb8a6c2564cf82ca3e1ff
zhhony <zhhony@126.com>
2022-11-26 23:07:50 +0800

    输出

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas
+import os
+
+path = os.environ['USERPROFILE'] + '\\desktop\\'
 
 pandas.set_option('display.unicode.east_asian_width', True)
 
@@ -8,11 +11,14 @@ pdErrorList = []
 
 for i in range(2011, 2022):
     for j in range(1, 13):
-            date = str(i) + str(j)
-            http = 'http://www.tianqihoubao.com/lishi/wuxi/month/%s.html' % date
-            try:
-                pdOutput.append(pandas.read_html(
-                    io=http, flavor='html5lib', encoding='gbk'))
-            except:
-                pdErrorList.append(date)
-                continue
+        date = str(i) + str(j)
+        http = 'http://www.tianqihoubao.com/lishi/wuxi/month/%s.html' % date
+        try:
+            pdList = pandas.read_html(
+                io=http, flavor='html5lib', encoding='gbk')
+            for pd in pdList:
+                pdOutput.append(pd)
+        except:
+            pdErrorList.append(date)
+            continue
+pandas.concat(pdOutput).to_excel(path + '123.xlsx')
