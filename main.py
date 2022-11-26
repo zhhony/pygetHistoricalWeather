302c3644ee0ec16e845c60159169785501d8be4d
zhhony <zhhony@126.com>
2022-11-26 22:00:02 +0800

    新增错误处理

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import pandas
 
 pandas.set_option('display.unicode.east_asian_width', True)
 
-pdOutput = pandas.read_html(io='http://www.tianqihoubao.com/lishi/wuxi/month/202211.html',flavor='html5lib',encoding='gbk')
+pdOutput = []
+pdErrorList = []
 
 
-for i in range(2011,2022):
-    for j in range(1,13):
-        date = str(i)  +  str(j)
-        http = 'http://www.tianqihoubao.com/lishi/wuxi/month/%s.html'%date
-        pd = pandas.read_html(io=http,flavor='html5lib',encoding='gbk')
+for i in range(2011, 2022):
+    for j in range(1, 13):
+            date = str(i) + str(j)
+            http = 'http://www.tianqihoubao.com/lishi/wuxi/month/%s.html' % date
+            try:
+                pdOutput.append(pandas.read_html(
+                    io=http, flavor='html5lib', encoding='gbk'))
+            except:
+                pdErrorList.append(date)
+                continue
